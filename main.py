8c1b0201cc85eac5ce0aaaf511827f46e4c70dcb
sahad sathar <sahadsathar1997@gmail.com>
2022-11-27 11:03:33 +0500

    completed

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from collections import Counter
 test_case = int (input("Enter the number of test cases "))
 count = 0
-counting ={}
 tweet_name = []
 while count < test_case:
     each_test_case = int(input("Enter the number of each test cases "))
     n = 0
     while each_test_case > n:
         tweet = (input("Enter user name and tweet id "))
-        tweet1 = tweet.split()
-        tweet_name.append(tweet1)
+        tweet_name.append(tweet)
         n += 1
     count += 1
-print (tweet_name)
-for i in tweet_name:
-    counting_names = Counter(i)
-print(counting_names)
-repeat = counting_names.values()
+uniq_names = [pref_names.split()[0] for pref_names in tweet_name]
+times = Counter(uniq_names)
 
+repeat = times.values()
 
+for element in set(repeat):
+    dupl = ([(key, value) for key, value in sorted(times.items()) if value == element])
+    if len(dupl) > 1:
+        for (key, value) in dupl:
+            print (key,'',value)
+    max_value = max(times.values())
+    temp_max_result = [(key, value) for key, value in sorted(times.items()) if value == max_value]
+
+    if temp_max_result != dupl:
+        for (key,value) in temp_max_result:
+            print (key,'',value)
