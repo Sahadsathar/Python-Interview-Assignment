458baa9aa6849ba02a4078916e849f3c418c2e57
sahad sathar <sahadsathar1997@gmail.com>
2022-11-27 10:29:18 +0500

    counting

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+from collections import Counter
 test_case = int (input("Enter the number of test cases "))
 count = 0
+counting ={}
 tweet_name = []
 while count < test_case:
     each_test_case = int(input("Enter the number of each test cases "))
     n = 0
     while each_test_case > n:
         tweet = (input("Enter user name and tweet id "))
-        tweet_name.append(tweet)
+        tweet1 = tweet.split()
+        tweet_name.append(tweet1)
         n += 1
     count += 1
 print (tweet_name)
+for i in tweet_name:
+    counting_names = Counter(i)
+print(counting_names)
+repeat = counting_names.values()
+
+
